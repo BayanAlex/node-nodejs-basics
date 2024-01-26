184632122e88acd34911fac54af3c23bca0aa3ba
Oleksandr Shyhyda <BayanAlex@gmail.com>
2024-01-27 01:23:32 +0200

    fix: args.js and transform.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,7 +3,7 @@ import process from 'process';
 const parseArgs = () => {
     const result = process.argv
         .filter((arg, index, args) => /^--/.test(arg) || (index > 0 && /^--/.test(args[index - 1])))
-        .reduce((res, value, index, arr) => res += index % 2 === 0 ? `${value} is ` : `${value}${index < arr.length - 1 ? ', ': ''}`,'');
+        .reduce((res, value, index, arr) => res += index % 2 === 0 ? `${value.slice(2)} is ` : `${value}${index < arr.length - 1 ? ', ': ''}`,'');
     console.log(result);
 };
 
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,8 +1,17 @@
 import process from 'process';
+import { Transform } from 'stream';
 
 const transform = async () => {
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        },
+    });
+    reverseTransform.setEncoding('ascii');
+    reverseTransform.on('data', chunck => process.stdout.write(chunck + '\n'));
+
     process.stdin.on('data', (data) => {
-        process.stdout.write(data);
+        reverseTransform.write(data);
     });
 };
 
