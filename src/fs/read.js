94b42c9f2b387d6a44d658794dfea66396ab15e4
Oleksandr Shyhyda <BayanAlex@gmail.com>
2024-01-27 13:57:50 +0200

    refactor: rewrote read of file using fs/promises

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { readFile } from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,15 +7,12 @@ const __dirname = path.dirname(__filename);
 
 const read = async () => {
     const file = path.resolve(__dirname, 'files', 'fileToRead.txt');
-    if (!fs.existsSync(file)) {
+    try {
+        const contents = await readFile(file, { encoding: 'utf8' });
+        console.log(contents);
+    } catch (error) {
         throw new Error('FS operation failed');
     }
-    fs.readFile(file, (error, data) => {
-        if (error) {
-            throw new Error('FS operation failed');
-        }
-        console.log(data.toString());
-    });
 };
 
 await read();
