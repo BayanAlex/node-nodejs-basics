31880e4cf156ec02e0f5ebb3b0185526568497db
Oleksandr Shyhyda <BayanAlex@gmail.com>
2024-01-30 00:22:15 +0200

    fix: added removing of paths from argv in args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,6 +2,7 @@ import process from 'process';
 
 const parseArgs = () => {
     const result = process.argv
+        .slice(2)
         .filter((arg, index, args) => /^--/.test(arg) || (index > 0 && /^--/.test(args[index - 1])))
         .reduce((res, value, index, arr) => res += index % 2 === 0 ? `${value.slice(2)} is ` : `${value}${index < arr.length - 1 ? ', ': ''}`,'');
     console.log(result);
